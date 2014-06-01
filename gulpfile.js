90832b3f5b828d14e4e488165ea6eda2614d346d
moznion <moznion@gmail.com>
2014-06-01 23:14:21 +0900

    Add gulp command

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,32 @@ var typescript = require('gulp-tsc');
 var flatten = require('gulp-flatten');
 var uglify = require('gulp-uglify');
 var bower = require ('gulp-bower-files');
+var clean = require ('gulp-clean');
+var Q = require('q');
+var exec = require('child_process').exec;
+var editJson = require('gulp-json-editor');
+var runSequence = require('gulp-run-sequence');
 
+function tag () {
+    var q = Q.defer();
+    exec('git describe --tags --always --dirty', function (err, stdout, stderr) {
+        if (err) {
+            q.reject(err);
+            return;
+        }
+
+        q.resolve(stdout.replace(/\n/, ''));
+    });
+    return q.promise;
+}
+
+function version () {
+    return tag().then(function (tag) {
+        return tag.replace(/-(\d+)/, '.$1')
+            .replace(/-g[0-9a-f]+/, '')
+            .replace(/-dirty/, '');
+    });
+}
 gulp.task('default', ['build']);
 
 gulp.task('typescript', function () {
@@ -15,13 +40,47 @@ gulp.task('typescript', function () {
         .pipe(gulp.dest('src/js'));
 });
 
+gulp.task('copy', ['copy-manifest', 'copy-src', 'copy-vendor']);
+
+gulp.task('copy-manifest', function () {
+    return gulp.src('src/manifest.json')
+        .pipe(gulp.dest('app'));
+});
+
+gulp.task('copy-src', function () {
+    return gulp.src('src/js/**/*.js')
+        .pipe(gulp.dest('app/js'));
+});
+
+gulp.task('copy-vendor', function () {
+    return gulp.src('vendor/*')
+        .pipe(gulp.dest('app/vendor'));
+});
+
 gulp.task('tsd', function () {
     return gulp.src('./gulp_tsd.json').pipe(tsd());
 });
 
 gulp.task('bower', function () {
     return bower().pipe(uglify({preserveComments:'some'}))
-                  .pipe(flatten())
-                  .pipe(gulp.dest('vendor'));
+        .pipe(flatten())
+        .pipe(gulp.dest('vendor'));
+});
+
+gulp.task('clean', function () {
+    return gulp.src('app')
+        .pipe(clean());
+});
+
+gulp.task('manifest', function () {
+    return version().then(function (version) {
+        return gulp.src('src/manifest.json')
+            .pipe(editJson({ version: version }))
+            .pipe(gulp.dest('app/'));
+    });
+});
+
+gulp.task('build', function () {
+    runSequence('typescript', 'copy');
 });
 
