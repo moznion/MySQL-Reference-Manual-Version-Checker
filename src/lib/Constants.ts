611279a1b64591f5ef48b7f48110ac48597d55cc
moznion <moznion@gmail.com>
2014-06-04 00:43:43 +0900

    Change default value

--- src/lib/Constants.ts
+++ src/lib/Constants.ts
@@ -1,6 +1,6 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
 class Constants {
-    static defaultFloorVersion : number = 0.0;
-    static defaultCapVersion : number = 999.0; // XXX local storage of chrome cannot recognize Infinity;
+    static defaultFloorVersion : number = 5.1;
+    static defaultCapVersion : number = 5.7;
 }
